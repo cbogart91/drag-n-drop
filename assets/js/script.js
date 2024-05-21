1b70238c98ee134760218fedd199affbcab5be4e
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-05-21 13:02:30 -0500

    created modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+const formEl = $('filledModal');
+const taskEl = $('taskTitle');
+const dueEl = $('taskDueDate');
+const descriptionEl = $('taskDescription');
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
@@ -9,7 +14,24 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+  const card =$(`
+  <div class="card${c} " id='${task[ID]}'>
+  <div class="card-body">
+  <h5 class ="card-title">${task[TITLE]}</h5>
+  <h6 class="card-subtitle">$dayjs(
+    dayjs(task[DUEDATE])
+  ).format('MM/DD/YYYY')}</h6>
+  <p class='card-text'>${task[DESCRIPTION]}</p>
+  <button class='btn-delete btn btn-primary" data-id="${task[ID]}">Delete</button>
+  </div>
+  </div>
+`)
+.data('text', task[ID])
+.draggable({
+  cursor: "move",
+  revert: true,
+});
+return card;
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -18,14 +40,27 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+const handleAddTask = function(event){
+  event.preventDefault();
+  const task = taskEl.val();
+  const due = dueEl.val();
+  const description = descriptionEl.val();
 
+  if (!task || !due || description) {
+    console.log("Please fill out the form completely!");
+    return;
+  }
+printCard(task, due, description);
 }
+
+formEl.on('submit', handleAddTask);
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
 
 }
+
+
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
@@ -36,3 +71,11 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
+$(function () {
+    $('#datepicker').datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  });
+
